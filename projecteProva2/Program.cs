ae17acbe0510c3ecfca6d0c6768e4e197841df1c
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2024-12-05 13:50:47 +0100

    ex3

--- projecteProva2/Program.cs
+++ projecteProva2/Program.cs
@@ -2,30 +2,44 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        const string MsgErrorDia = "Introdueix el dia";
+        const string MsgErrorMes = "Introdueix el mes";
+        const string MsgErrorAny = "Introdueix el any";
+        public static void Main(string[] args)
         {
             int dia = 0;
             int mes = 0;
             int any = 0;
-            const string Msg = "Introdueix el dia, mes i any";
-            const string MsgErrorDia = "Introdueix el dia";
-            const string MsgErrorMes = "Introdueix el mes";
-            const string MsgErrorAny = "Introdueix el any";
 
-            Console.WriteLine(Msg);
+
+            Console.WriteLine(MsgErrorDia);
             dia = Int16.Parse(Console.ReadLine());
+            Console.WriteLine(MsgErrorMes);
             mes = Int16.Parse(Console.ReadLine());
+            Console.WriteLine(MsgErrorAny);
             any = Int16.Parse(Console.ReadLine());
+            DiaCorrecte(dia);
+            MesCorrecte(mes);
+            AnyCorrecte(any);
+        }
+        public static void DiaCorrecte(int dia)
+        {
             while (dia < 1 || dia > 31)
             {
                 Console.WriteLine(MsgErrorDia);
                 dia = Int16.Parse(Console.ReadLine());
             }
+        }
+        public static void MesCorrecte(int mes)
+        {
             while (mes < 1 || mes > 12)
             {
                 Console.WriteLine(MsgErrorMes);
                 mes = Int16.Parse(Console.ReadLine());
             }
+        }
+        public static void AnyCorrecte(int any)
+        {
             while ((any % 400 == 0) || ((any % 4 == 0) && (any % 100 != 0)))
             {
                 Console.WriteLine(MsgErrorAny);
