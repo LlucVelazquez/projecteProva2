b2eec4678f2c8a9b9b1ea79931f780595ffe5297
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2024-12-05 13:30:39 +0100

    ex2

--- projecteProva2/Program.cs
+++ projecteProva2/Program.cs
@@ -7,9 +7,31 @@ namespace projecteProva2
         static void Main(string[] args)
         {
             int num = 0;
+            int n = 5;
+            int y = 0;
             num = Int16.Parse(Console.ReadLine());
             int[] ints = { 10, 4, 6, 4, 8, -13, 2, 3 };
-            SearchClass.;
+            
+            isNumberList(ints, num);
+
+            SecondSort.Order(ints, n, y);
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.Write($"{ints[i]} ");
+            }
+        }
+        public static void isNumberList(int[] arrs, int num) // No se quina llibreria es per fer la a
+        {
+            const string IsInList = "El Numero esta en la llista";
+
+            for (int i = 0; i < arrs.Length; i++)
+            {
+                if (num == arrs[i])
+                {
+                    Console.WriteLine(IsInList);
+                }
+            }
+
         }
     }
 }
