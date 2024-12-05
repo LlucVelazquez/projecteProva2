7d6840b0a81c334e1475a8284e96dc0a2333158a
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2024-12-05 13:04:31 +0100

    casi ex5

--- projecteProva2/Program.cs
+++ projecteProva2/Program.cs
@@ -2,9 +2,53 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            cosnt string Msg = "Introdueix els 20 numeros per separat: ";
+            const string DemanarNum = "Introdueix un nombre: ";
+            const string 
+            int[] arrs = new int[20];
+            int num = 0;
+
+            Console.WriteLine(Msg);
+            for(int i = 0; i < arrs.Length; i++)
+            {
+                arrs[i] = Int16.Parse(Console.ReadLine());
+            }
+
+            BubbleSort(arrs);
+
+            for (int i = 0;i < arrs.Length; i++)
+            {
+                Console.Write($"{arrs[i]} ");
+            }
+
+            Console.WriteLine(DemanarNum);
+            num = Int16.Parse(Console.ReadLine());
+            Console.WriteLine(isNumberList(arrs, num);
+        }
+        public static void BubbleSort(int[] arrs)
+        {
+            for (int i=0; i < arrs.Length - 1; i++) 
+            {
+                for (int j = i + 1; j < arrs.Length; j++)
+                {
+                    if (arrs[i] < arrs[j])
+                    {
+                        int aux = arrs[i];
+                        arrs[i] = arrs[j];
+                        arrs[j] = aux;
+                    }
+                }
+            }
+        }
+        public static bool isNumberList(int[] arrs, int num)
+        {
+            for ( int i = 0; i < arrs.Length; i++)
+            {
+                if (num == arrs[i]) return true;
+            }
+            return false;
         }
     }
 }
